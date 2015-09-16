8fe577e1bf21f97663c1822c043b9e413546ed6e
Jennifer Folkerth <jfolkerth@pillartechnology.com>
2015-09-16 16:04:24 -0400

    added spike code

---
+++ test/test.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Base Test', function () {
+
+    beforeEach(function () {
+    });
+
+    it('should pass', function() {
+        expect(5).to.equal(5);
+    });
+
+});
