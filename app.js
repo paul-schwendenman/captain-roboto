8a1ac36e90ebce5bfdfa9e26782d6cd6c3e45564
ralphhale <ralphhale@gmail.com>
2015-09-17 14:53:29 -0400

    set __base.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+// set base (root) directory
+global.__base = __dirname + '/';
+
 var app = express();
 var bot = require('./src/bot');
 
