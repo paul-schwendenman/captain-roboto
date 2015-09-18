bae5aaac35ec429a5f13ebf24a40a75c29e2a32f
ralphhale <ralphhale@gmail.com>
2015-09-18 11:15:29 -0400

    Adeed eight ball bot to routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 var dice_bot = require(baseDir() + 'src/dice_bot/dice_bot.js');
 var rps_bot = require(baseDir() + 'src/rps_bot/rps_bot.js');
+var eight_ball_bot = require(baseDir() + 'src/eight_ball_bot/eight_ball_bot.js');
 
 module.exports = {
     routeTable: {
+        '!eightball': eight_ball_bot,
         '!roll': dice_bot,
         '!rps': rps_bot
     }
