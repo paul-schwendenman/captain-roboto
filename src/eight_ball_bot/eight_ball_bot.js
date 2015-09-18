a06abaec4b0db724fdeecb37c837113ddef5b10b
ralphhale <ralphhale@gmail.com>
2015-09-18 11:39:24 -0400

    Pulled the response picker logic into its own module

--- src/eight_ball_bot/eight_ball_bot.js
+++ src/eight_ball_bot/eight_ball_bot.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var defs = require(__base + 'src/eight_ball_bot/eight_ball_bot_defs');
+var response_picker_bot = require(__base + 'src/response_picker_bot/response_picker_bot');
 
 module.exports = {
     handleMessage: handleMessage,
@@ -12,15 +13,13 @@ function handleMessage(message, randomFn) {
 
     var tokens = message.text.split(' ', 2);
     if (tokens.length > 1) {
-         return generateResponse(randomFn);
+         return generateResponse(defs.options, randomFn);
 
     }
     return defs.noArgumentMessage;
 }
 
-function generateResponse(randomFn) {
-    randomFn = randomFn || Math.random;
-
-    return defs.options[Math.floor((randomFn() * defs.options.length))];
+function generateResponse(options, randomFn) {
+    return response_picker_bot.generateResponse(options, randomFn);
 }
 
--- test/eight_ball_bot/eight_ball_bot_test.js
+++ test/eight_ball_bot/eight_ball_bot_test.js
@@ -6,10 +6,8 @@ var eight_ball_bot = require(baseDir() + 'src/eight_ball_bot/eight_ball_bot');
 var defs = require(baseDir() + 'src/eight_ball_bot/eight_ball_bot_defs');
 var expect = require('chai').expect;
 
-var fakeRandomFn = function(index) {
-    return function () {
-        return index/defs.options.length;
-    };
+var fakeRandomFn = function() {
+    return 0;
 };
 
 
@@ -30,7 +28,7 @@ describe('Eight Ball Bot', function() {
             text: '!eightball Is this best project ever?'
         };
 
-        var response = eight_ball_bot.handleMessage(message, fakeRandomFn(0));
+        var response = eight_ball_bot.handleMessage(message, fakeRandomFn);
 
         expect(response).to.equal(defs.options[0]);
 
@@ -38,16 +36,3 @@ describe('Eight Ball Bot', function() {
 
 });
 
-describe('Generate options', function() {
-    it('returns a choice from the list of options', function() {
-        var response = eight_ball_bot.generateResponse(fakeRandomFn(0));
-
-        expect(response).to.equal(defs.options[0]);
-    });
-
-    it('returns the 3rd choice from the list of options', function() {
-       var response = eight_ball_bot.generateResponse(fakeRandomFn(2));
-
-        expect(response).to.equal(defs.options[2]);
-    });
-});
