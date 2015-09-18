993083fe475cad1d0f68ea68804a364d61654ee5
d-- <d--@users.noreply.github.com>
2015-09-18 13:50:55 -0400

    Check for an API key argument.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
-
 var Slack = require('slack-client');
-var apiToken = process.argv[2];
+var apiToken = '';
+
+if (process.argv.length < 3) {
+    console.error("Please specify an API key.");
+} else {
+    apiToken = process.argv[2];
+}
 
 // set base (root) directory
 global.__base = __dirname + '/';
